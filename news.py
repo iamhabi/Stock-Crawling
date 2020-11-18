f066470a439c63b9ca1fdf6e089c0049f4c0ddd6
iamhabi <chanvinhong@gmail.com>
2020-11-18 16:54:10 +0900

    stock

--- news.py
+++ news.py
@@ -52,3 +52,40 @@ def get_news(code):
             if i:
                 f.write("%s\n" % i)
         f.write("-----------------------------\n")
+
+def get_n(code):
+    newsURL = "https://finance.naver.com/item/news_news.nhn?code=" + code + "&page=&sm=title_entity_id.basic&clusterId="
+
+    result = requests.get(newsURL)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    table = soup.find('table', {'class':'type5'}).find('tbody')
+
+    tr = table.find_all('tr')
+
+    news = []
+    flag = 0
+
+    for i in range(len(tr)):
+        n = []
+
+        if tr[i].get('class'):
+            a = tr[i].get('class')[0]
+            if a == "relation_lst":
+                flag = 1
+            elif a == "hide_news":
+                continue
+            else:
+                n.append(tr[i].find('a').text)
+                n.append(tr[i].find('td', {'class':'date'}).text)
+                n.append(tr[i].find('a').get('href'))
+        else:
+            if flag == 1:
+                flag = 0
+            else:
+                n.append(tr[i].find('a').text)
+                n.append(tr[i].find('td', {'class':'date'}).text)
+                n.append(tr[i].find('a').get('href'))
+
+        news.append(n)
+
+    return news
--- sise_day.py
+++ sise_day.py
@@ -44,3 +44,37 @@ def get_sise_day(code):
         for i in stock_day:
             f.write("%s\n" % i)
         f.write("---------------\n")
+
+def get_sd(code):
+    result = requests.get(sisedayURL + code)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    table = soup.find('table', {'class':'type2'})
+
+    th = table.find_all('th')
+
+    cate = []
+
+    for i in range(len(th)):
+        if i == 2:
+            cate.append(th[i].text)
+            cate.append('상승/하락')
+        else:
+            cate.append(th[i].text)
+
+    stock_day = []
+
+    tr = table.find_all('tr', {'onmouseover':'mouseOver(this)'})
+
+    for i in range(len(tr)):
+        day = []
+        td = tr[i].find_all('td')
+
+        for j in range(len(td)):
+            if j == 2:
+                day.append(td[j].text.strip())
+            else:
+                day.append(td[j].text)
+        
+        stock_day.append(day)
+
+    return stock_day
